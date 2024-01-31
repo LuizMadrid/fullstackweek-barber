7e01b86a293fede88a6a74c124c303e5d82db0d1
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-01-30 21:07:55 -0300

    chore: add initial prisma schema again.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "tailwindcss";
 
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
