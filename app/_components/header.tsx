373230920a83b2e5c73b85d40ae8d8b389b43f9d
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-01-30 23:26:57 -0300

    feat: add new components from shadcn.

---
+++ app/_components/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { Card } from './ui/card';
+import { Button } from './ui/button';
+import { MenuIcon } from 'lucide-react';
+
+export const Header = () => {
+	return (
+		<>
+			<Card className="justify-between flex items-center p-5">
+				<Image 
+					src="/FSW Barber.png" 
+					alt="FSW Barber" 
+					width={120} 
+					height={18} 
+				/>
+
+				<Button variant={'outline'} size={'icon'} className='size-8'>
+					<MenuIcon size={16} />
+				</Button>
+				
+			</Card>
+		</>
+	);
+};
