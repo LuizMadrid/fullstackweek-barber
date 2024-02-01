20bf3432dac8155b31e3a35641c6f700eaeee31f
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-01-31 23:08:52 -0300

    feat: add initial booking component.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,30 +4,39 @@ import { Header } from '../_components/header';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Search } from './_components/search';
+import { BookingItem } from '../_components/booking-item';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 
-			<div className='flex flex-col gap-2 px-5 pt-5'>
-				<div className='flex gap-2 text-xl font-bold'>
-					<span className='font-normal'>Olá,</span>
-					<h2>&apos;nome do usuário&apos;!</h2>
+			<div className='flex flex-col gap-6'>
+				<div className='flex flex-col gap-2 px-5 pt-5'>
+					<div className='flex gap-2 text-xl font-bold'>
+						<span className='font-normal'>Olá,</span>
+						<h2>&apos;nome do usuário&apos;!</h2>
+					</div>
+					<p className='flex gap-1 capitalize'>
+						{format(new Date(), 'EEEE\',\' d', {
+							locale: ptBR,
+						})}
+						<span className='normal-case'>de</span>
+						{format(new Date(), 'MMMM', {
+							locale: ptBR,
+						})}
+					</p>
 				</div>
-				<p className='flex gap-1 capitalize'>
-					{format(new Date(), 'EEEE\',\' d', {
-						locale: ptBR,
-					})}
-					<span className='normal-case'>de</span>
-					{format(new Date(), 'MMMM', {
-						locale: ptBR,
-					})}
-				</p>
-			</div>
 
-			<div className='px-5 max-w-xl mt-6'>
-				<Search />
+				<div className='px-5 max-w-xl'>
+					<Search />
+				</div>
+
+				<div className='flex flex-col gap-2 px-5 max-w-xl'>
+					<h2 className='text-lg uppercase text-gray-400'>Agendamentos</h2>
+					<BookingItem />
+				</div>
+
 			</div>
 		</>
 	);
---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Card, CardContent } from './ui/card';
+import { Badge } from './ui/badge';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+export const BookingItem = () => {
+	return (
+		<>
+			<Card>
+				<CardContent className='flex p-0'>
+					<div className='flex flex-[3] flex-col gap-2 p-5'>
+						<Badge className='bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit'>Confirmado</Badge>
+
+						<h2 className='text-lg font-bold'>Corte de Cabelo</h2>
+						
+						<div className='flex text-center items-center gap-2'>
+							<Avatar className='size-8'>
+								<AvatarImage src='https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png' />
+								<AvatarFallback>N/A</AvatarFallback>
+							</Avatar>
+
+							<h3>Vintage Barber</h3>
+						</div>
+					</div>
+
+					<div className='flex flex-[1] flex-col items-center justify-center border-l border-secondary p-5'>
+						<p>Fevereiro</p>
+						<p className='text-4xl'>06</p>
+						<p>09:45</p>
+					</div>
+				</CardContent>
+			</Card>
+		</>
+	);
+};
