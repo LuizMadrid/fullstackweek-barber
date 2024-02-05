691f31ad556fd84ccab51f224432263c755b46c7
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-02-05 16:09:03 -0300

    feat: create a dynamic component to display user name's on login.

---
+++ app/(home)/_components/welcome-user.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+
+export const WelcomeForUser = () => {
+
+	const {data, status} = useSession();
+  
+	return (
+		<div className='flex gap-2 text-xl font-bold'>
+			<span className='font-normal'>Olá,</span>
+
+			{status === 'unauthenticated' && (
+				<h2>Faça seu Login!</h2>
+			)}
+
+			{status === 'authenticated' && (
+				<h2>{data?.user?.name}!</h2>
+			)}
+		</div>
+	);
+};
